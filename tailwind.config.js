4e3aae436050d50407e9ddbae069d5f7abff611e
LandonRGeorge <landon.robert.george@gmail.com>
2021-12-07 15:03:31 -0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero': "url('../assets/mobile/image-host.jpg')",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
