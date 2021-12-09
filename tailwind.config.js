122268f86259469aab9cf02481f0de1174fadc22
LandonRGeorge <landon.robert.george@gmail.com>
2021-12-09 13:53:23 -0600

    More work

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
         'hero': "url('../assets/mobile/image-host.jpg')",
       }
     },
+    colors: {
+      green: {
+        100: '#54e6af',
+        200: '#B3FFE2'
+      },
+      blue: {
+        100: '#c2cbe5',
+        200: '#5a668a',
+        300: '#2c344b',
+        400: '#121725'
+      },
+      white: '#fff',
+      black: '#000',
+      red: '#FB3E3E'
+    }
   },
   variants: {
     extend: {},
